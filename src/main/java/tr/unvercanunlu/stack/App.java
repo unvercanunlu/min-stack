7be7bcd21ce84db4a8c21cd96a3cd399b1d52715
unver <unvercanunlu@gmail.com>
2025-09-20 02:28:00 +0200

    log provider added.

--- src/main/java/tr/unvercanunlu/stack/App.java
+++ src/main/java/tr/unvercanunlu/stack/App.java
@@ -2,16 +2,36 @@ package tr.unvercanunlu.stack;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
+import tr.unvercanunlu.stack.example.IntegerStack;
 import tr.unvercanunlu.stack.example.Product;
 import tr.unvercanunlu.stack.example.ProductMinMaxStack;
 import tr.unvercanunlu.stack.service.impl.BaseMinMaxStack;
+import tr.unvercanunlu.stack.service.impl.BaseStack;
 
+@Slf4j
 public class App {
 
-  public static void main(String[] args) {
-    // BaseStack<Integer> integerStack = new IntegerStack();
+  private static void basicExample() {
+    BaseStack<Integer> stack = new IntegerStack();
 
-    BaseMinMaxStack<Product> productMinMaxStack = new ProductMinMaxStack();
+    stack.push(1);
+    log.info("Operation 'peek': {}", stack.peek());
+
+    log.info("Operation 'pop': {}", stack.pop());
+    log.info("Operation 'pop': {}", stack.pop());
+
+    stack.push(2);
+    stack.push(3);
+    log.info("Operation 'pop': {}", stack.pop());
+
+    stack.clear();
+    log.info("Operation 'peek': {}", stack.peek());
+    log.info("Operation 'size': {}", stack.size());
+  }
+
+  private static void minMaxExample() {
+    BaseMinMaxStack<Product> minMaxStack = new ProductMinMaxStack();
 
     List<Product> products = List.of(
         new Product("product-1", 5),
@@ -24,18 +44,23 @@ public class App {
         new Product("product-8", 4)
     );
 
-    products.forEach(productMinMaxStack::push);
+    products.forEach(minMaxStack::push);
 
     int steps = products.size() + 3;
 
     IntStream.range(1, steps)
         .forEach(step -> {
-          System.out.println("Step " + step + ":");
-          System.out.println("Operation 'peek': " + productMinMaxStack.peek());
-          System.out.println("Operation 'peek min': " + productMinMaxStack.peekMin());
-          System.out.println("Operation 'peek max': " + productMinMaxStack.peekMax());
-          System.out.println("Operation 'pop': " + productMinMaxStack.pop());
+          log.info("Step {}:", step);
+          log.info("Operation 'peek': {}", minMaxStack.peek());
+          log.info("Operation 'peek min': {}", minMaxStack.peekMin());
+          log.info("Operation 'peek max': {}", minMaxStack.peekMax());
+          log.info("Operation 'pop': {}", minMaxStack.pop());
         });
+  }
+
+  public static void main(String[] args) {
+    basicExample();
+    minMaxExample();
   }
 
 }
